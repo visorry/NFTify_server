2eed6c9c1d66c78118297efa59754cddfc7df9da
visorry <vishnuvisa00@gmail.com>
2024-02-16 13:14:27 +0530

    added cors middleware

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const connect = require('./config/db')
+const cors = require('cors');
 const authRoutes = require('./routes/authRoute');
 const nftRoutes = require('./routes/ntfRoutes')
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(cors())
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/' , nftRoutes);
